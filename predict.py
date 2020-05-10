e35138acf3250b008c7036fd71eb082b9b9b6e3e
Jeff Charlesworth <egboh.catherine@utexas.edu>
2020-05-10 13:58:37 -0500

    Adding model and updating predict file

--- predict.py
+++ predict.py
@@ -8,7 +8,7 @@ def predict_from_csv(path_to_csv):
     df = pd.read_csv(path_to_csv)
     X, y = prep_data(df)
 
-    reg = load("reg.joblib")
+    reg = load("model_joblib")
 
     predictions = reg.predict(X)
 
@@ -16,14 +16,14 @@ def predict_from_csv(path_to_csv):
 
 if __name__ == "__main__":
     predictions = predict_from_csv("fish_holdout_demo.csv")
-    # print(predictions)
+    print(predictions)
 ######
 
 ### WE WRITE THIS ###
-    from sklearn.metrics import mean_squared_error
-    ho_predictions = predict_from_csv("fish_holdout.csv")
-    ho_truth = pd.read_csv("fish_holdout.csv")["Weight"].values
-    ho_mse = mean_squared_error(ho_truth, ho_predictions)
-    print(ho_mse)
+    # from sklearn.metrics import mean_squared_error
+    # ho_predictions = predict_from_csv("fish_holdout.csv")
+    # ho_truth = pd.read_csv("fish_holdout.csv")["Weight"].values
+    # ho_mse = mean_squared_error(ho_truth, ho_predictions)
+    # print(ho_mse)
 ######
 
