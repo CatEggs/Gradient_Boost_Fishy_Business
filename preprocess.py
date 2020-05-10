b0d96f29f21bb7cf05a14e3ffe4d9a41d376bc1d
Jeff Charlesworth <egboh.catherine@utexas.edu>
2020-05-10 16:03:16 -0500

    Final draft. Updated preprocess.py to take in more parameters and reran model

--- preprocess.py
+++ preprocess.py
@@ -1,8 +1,6 @@
 def prep_data(df):
 
-    df = df.assign(hw=df["Height"] * df["Width"])
-
-    X = df[["Height", "Width", "hw"]].values
+    X = df[["Length1", "Length2", "Length3", "Height", "Width"]].values
     y = df["Weight"].values
 
     return X, y
